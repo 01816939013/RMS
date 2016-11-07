634ca62689fc5b0628eb7863d6293dcd8aef4ced
DESKTOP-7NQ67K9\elsayed <elsayed.fcis.1@gmail.com>
2016-11-07 21:00:07 +0200

    insert records in db tables by using model factory and install laravel collective

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,12 +12,87 @@
 */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'role' => $faker->word,
+        'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Menu::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => $faker->sentence,
+        'type' => $faker->word,
+        'description' => $faker->text,
+        'status' => $faker->boolean,
+        'image' => $faker->imageUrl($width=640,$height=300),
+        'user_id' => $faker->numberBetween($min = 1, $max = 125),
+    ];
+});
+
+
+$factory->define(App\Item::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => $faker->sentence,
+        'price' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 100),
+        'description' => $faker->text,
+        'status' => $faker->boolean,
+        'image' => $faker->imageUrl($width=640,$height=300),
+        'user_id' => $faker->numberBetween($min = 1, $max = 125),
+        'menu_id' => $faker->numberBetween($min = 1, $max = 50),
+    ];
+});
+
+$factory->define(App\Meal::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => $faker->sentence,
+        'price' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 100),
+        'description' => $faker->text,
+        'status' => $faker->boolean,
+        'image' => $faker->imageUrl($width=640,$height=300),
+        'user_id' => $faker->numberBetween($min = 1, $max = 10),
+    ];
+});
+
+$factory->define(App\Customer::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'address' => $faker->address,
+        'city' => $faker->city,
+        'phone' => $faker->e164PhoneNumber,
+        'password' => bcrypt(str_random(10)),
+    ];
+});
+
+$factory->define(App\Order::class, function (Faker\Generator $faker) {
+
+    return [
+        'total' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 100),
+        'cashIn' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 100),
+        'payment' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 100),
+        'change' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 10, $max = 100),
+        'status' => $faker->boolean,
+        'customer_id' => $faker->numberBetween($min = 1, $max = 100),
+    ];
+});
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->text,
+        'status' => $faker->boolean,
+        'image' => $faker->imageUrl($width=640,$height=300),
+        'customer_id' => $faker->numberBetween($min = 1, $max = 30),
+        'order_id' => $faker->numberBetween($min = 1, $max = 100),
+        'rate' => $faker->numberBetween($min = 1, $max = 5),
     ];
 });
